eb8afe00fa5ca868652ed3eccf61d7d38a50b177
Boruto <bollam90205@gmail.com>
2020-05-31 19:48:11 +0800

    add some unknown change

--- ithome/ithome/urls.py
+++ ithome/ithome/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('welcome.urls')),
+    path('welcome', include('welcome.urls')),
 ]
